f8760805f531c223f8dd34f70f945bf8b0750b41
Juan Pablo <juampi56@gmail.com>
2020-05-10 04:48:21 -0300

    Perfiles como otra auth????

--- app/Perfil.php
+++ app/Perfil.php
@@ -3,9 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Perfil extends Model
+class Perfil extends Authenticatable
 {
+    protected $guard = 'profile';
+
+    protected $fillable = [
+        'name',
+    ];
+
     protected $table = 'perfiles'; // Esto es porque si no busca la table 'perfils' porque hace el plural en ingles.
     public function user(){
         return $this->belongsTo(User::class); //Pertenece a un Usuario.
