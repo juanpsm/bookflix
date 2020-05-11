cffa45f9c46aa7207e63930c1040fa67d13027b3
Juan Pablo <juampi56@gmail.com>
2020-05-11 02:57:13 -0300

    arreglo nombres de tablas

--- database/migrations/2020_05_11_035307_create_autores_table.php
+++ database/migrations/2020_05_11_035307_create_autores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAutorsTable extends Migration
+class CreateAutoresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -27,6 +27,6 @@ class CreateAutorsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('autors');
+        Schema::dropIfExists('autores');
     }
 }
