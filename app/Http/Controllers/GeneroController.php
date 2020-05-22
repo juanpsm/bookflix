e72d85dd0470b9f6f0594865491d989141fa1555
Juan Pablo <juampi56@gmail.com>
2020-05-22 08:44:55 -0300

    Arreglo lo de login en genero

--- app/Http/Controllers/GeneroController.php
+++ app/Http/Controllers/GeneroController.php
@@ -14,6 +14,7 @@ class GeneroController extends Controller
         
         //agrego los admin, los users se hacen por otro lado
         $this->middleware('auth', ['only' => ['showGenero']]);
+        $this->middleware('auth:admin', ['except' => ['showGenero']]);
     
     }
     /**
