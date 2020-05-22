b9012c1e489bd4376bb10c493e078c38b8583bfd
Juan Pablo <juampi56@gmail.com>
2020-05-21 22:04:00 -0300

    libros genero html de netflix y estilos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,10 +18,11 @@
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/nuestro.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
                 <div class="container">
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Bookflix') }}
