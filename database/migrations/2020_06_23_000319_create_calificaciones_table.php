7a17b37119d0060a822725eb7f4c397f2b097749
pibytes <juampi56@gmail.com>
2020-06-23 02:52:23 -0300

    Reseñas (la cuarta es la vencida)

--- database/migrations/2020_06_23_000319_create_calificaciones_table.php
+++ database/migrations/2020_06_23_000319_create_calificaciones_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCalificacionsTable extends Migration
+class CreateCalificacionesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCalificacionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('calificacions', function (Blueprint $table) {
+        Schema::create('calificaciones', function (Blueprint $table) {
             $table->id();
             $table->integer('puntaje');
             $table->integer('perfil_id');
@@ -29,6 +29,6 @@ class CreateCalificacionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('calificacions');
+        Schema::dropIfExists('calificaciones');
     }
 }
