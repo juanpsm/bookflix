549cbb2b610d0bb0ad843a32ec1ea6d8ff3e7757
pibytes <juampi56@gmail.com>
2020-07-15 13:37:14 -0300

    Bug volver

--- resources/views/capitulos/mostrarPDFuser.blade.php
+++ resources/views/capitulos/mostrarPDFuser.blade.php
@@ -5,22 +5,29 @@
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card bg-dark">
-        <div class="card-header d-flex align-items-center justify-content-between">
-          <div class="p-2">
-            <span> {{$libro->titulo}} - {{$capitulo->titulo}} </span>
-            @if($leido)
-            <svg class="bi bi-check-all text-primary" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M8.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14l.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z"/>
-            </svg>
+        <div class="card-header d-flex justify-content-between">
+          <span class="p-2">
+            {{$libro -> titulo}}
+            @if ($libro->esPorCapitulos())
+              - {{$capitulo->titulo}}
             @endif
-          </div>
-          <div class="d-flex justify-content-end">
+            @if($leido)
+              <svg class="bi bi-check-all text-primary" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14l.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z"/>
+              </svg>
+            @endif
+          </span>
+          <div class="col-5 d-flex">
             @if(!$leido)
             <a href="{{route('capitulo.marcarLeido', ['libro_id'=>$libro->id, 'id'=>$capitulo->id])}}" class= "btn btn-primary ml-auto" >
               Marcar como leido
             </a>
             @endif
-            <a href="{{route('libro.capitulos', $libro->id)}}" class= "btn btn-secondary ml-auto" >
+            @if ($libro->esPorCapitulos())
+              <a href="{{route('libro.capitulos', $libro->id)}}" class= "btn btn-secondary ml-auto" >
+            @else
+              <a href="{{route("libros.showForUser", $libro)}}" class= "btn btn-secondary ml-auto">
+            @endif
               Volver
             </a>
           </div>
